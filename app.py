9aca34a86abc76a8949f21148bf83ec6793e90d1
James <jamesinsheffield@hotmail.com>
2019-06-29 22:05:23 +0100

    Add config

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
-
+import os
 
 app = Flask(__name__)
+assert "APP_SETTINGS" in os.environ, "APP_SETTINGS environment variable not set"
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route("/")
 def hello():
     return "<h1>Hello World!</h1>"
-    
+
 if __name__ == "__main__":
     app.run()
