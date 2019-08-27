03659f85708186c32f60bcae442ab69ec46cdcea
James <jamesinsheffield@hotmail.com>
2019-08-27 20:28:25 +0100

    Add countries

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask
 import os
+import pandas as pd
+import random as rd
 
 app = Flask(__name__)
 assert "APP_SETTINGS" in os.environ, "APP_SETTINGS environment variable not set"
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+def ReadWordsCSV():
+    Words = pd.read_csv('words.csv')
+    return Words
+
+
 @app.route("/")
 def hello():
-    return "<h1>Hello World!</h1>"
+    Words = ReadWordsCSV()
+    idx = list(range(len(Words)))
+    rd.shuffle(idx)
+    Eng = Words.loc[idx[0],'English']
+    Rom = Words.loc[idx[0],'Romanian']
+    str = "<h1>"+Eng+"</h1><h1>"+Rom+"</h1>"
+    return str
 
 if __name__ == "__main__":
     app.run()
